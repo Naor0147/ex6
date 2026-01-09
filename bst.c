ca2eaf0f990001105bc217c907cbdc878b8b7823
Naor0147 <naoragam0147@gmail.com>
2026-01-09 17:24:06 +0000

    No changes detected

--- bst.c
+++ bst.c
@@ -4,5 +4,6 @@
 
 
 BST* createBST(int (*cmp)(void*, void*), void (*print)(void*), void (*freeData)(void*)){
-    BST * newTree=(BST * )malloc(sizeof(BST));//creates the tree in the heap 
+    BST * newTree=(BST * )malloc(sizeof(BST));//allcoating meomry to the tree in the heap 
+    ;
 }
