4f9e55ccdcea0cf955c3e069d06d8fecdf764653
Naor0147 <naoragam0147@gmail.com>
2026-01-09 19:29:50 +0000

    No changes detected

--- bst.c
+++ bst.c
@@ -27,10 +27,9 @@ BSTNode *bstInsert(BSTNode *root, void *data, int (*cmp)(void *, void *))
         return newNode;
     }
 
-    int value = cmp(root->data, data);//return postive if left > right ,negtive if left<right , 0 left==right 
+    int value = cmp(root->data, data); // return postive if left > right ,negtive if left<right , 0 left==right
 
-
-    if (value >0)
+    if (value > 0)
     {
         root->left = bstInsert(root->left, data, cmp);
     }
@@ -80,35 +79,28 @@ void deleteTree(BST *tree)
     free(tree);
 }
 
-
-
 void *bstFind(BSTNode *root, void *data, int (*cmp)(void *, void *))
 {
-    //if data was'nt found
+    // if data was'nt found
     if (root == NULL)
     {
-        return;
+        return NULL;
     }
 
-    int value = cmp(root->data, data);//return postive if left > right ,negtive if left<right , 0 left==right 
-    if (value==0)//found
+    int value = cmp(root->data, data); // return postive if left > right ,negtive if left<right , 0 left==right
+    if (value == 0)                    // found
     {
-        return root;
+        return root->data;
     }
 
-    if (value >0)
+    if (value > 0)
     {
-        //data is smaller then root data 
-        return bstFind(root->left,data,cmp);
+        // data is smaller then root data
+        return bstFind(root->left, data, cmp);
     }
-    //data is bigger than root data 
-    return bstFind(root->left,data,cmp);
-    
-    
-
+    // data is bigger than root data
+    return bstFind(root->right, data, cmp);
 }
-
-
 
 void bstInorder(BSTNode *root, void (*print)(void *));
 void bstPreorder(BSTNode *root, void (*print)(void *));
