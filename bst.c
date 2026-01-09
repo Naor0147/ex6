8f0fcee83e0c8d373414d34a0a807b9f5061aad2
Naor0147 <naoragam0147@gmail.com>
2026-01-09 17:47:33 +0000

    No changes detected

--- bst.c
+++ bst.c
@@ -1,32 +1,67 @@
 #include <stdlib.h>
 #include "bst.h"
 
-//Creates the Binary Tree , get compare ,print and free data Functions,set the root to null
+// Creates the Binary Tree , get compare ,print and free data Functions,set the root to null
 BST *createBST(int (*cmp)(void *, void *), void (*print)(void *), void (*freeData)(void *))
 {
     BST *newTree = (BST *)malloc(sizeof(BST)); // allcoating meomry to the tree in the heap
 
-    //Initialize the fields
+    // Initialize the fields
     newTree->compare = cmp;
     newTree->print = print;
     newTree->freeData = freeData;
-    newTree->root = NULL; //the root start as empty
+    newTree->root = NULL; // the root start as empty
     return newTree;
 }
 
-
-//Delete root Data and free memory
-void bstFree(BSTNode* root, void (*freeData)(void*))
+// Delete root Data and free memory
+void bstFree(BSTNode *root, void (*freeData)(void *))
 {
-    freeData(root->data);
+    if (root == NULL)
+    {
+        return;
+    }
+
+    if (root->data != NULL)
+    {
+        freeData(root->data);
+    }
+
+    if (root->left != NULL)
+    {
+        free(root->left);
+        root->left = NULL;
+    }
+    if (root->right != NULL)
+    {
+        free(root->right);
+        root->right = NULL;
+    }
+
     free(root);
 }
 
-
-
-//Deletes The Tree and frees all the memory
-void DeleteTree(BST *tree)
+// free all the nodes
+void freeAllNodes(BST *tree)
 {
+    if (tree == NULL)
+    {
+        return;
+    }
+    if (tree->root)
+    {
+        /* code */
+    }
+    
+    freeAllNodes(tree->root->left);
+    freeAllNodes(tree->root->right);
 
+    bstFree((tree->root), tree->freeData);
+}
+
+// Deletes The Tree and frees all the memory
+void deleteTree(BST *tree)
+{
+    freeAllNodes(tree);
     free(tree);
 }
--- bst.h
+++ bst.h
@@ -1,26 +1,30 @@
 #ifndef BST_H
 #define BST_H
 
-typedef struct BSTNode {
-    void* data;
-    struct BSTNode* left;
-    struct BSTNode* right;
+typedef struct BSTNode
+{
+    void *data;
+    struct BSTNode *left;
+    struct BSTNode *right;
 } BSTNode;
 
-typedef struct {
-    BSTNode* root;
-    int (*compare)(void*, void*);
-    void (*print)(void*);
-    void (*freeData)(void*);
+typedef struct
+{
+    BSTNode *root;
+    int (*compare)(void *, void *);
+    void (*print)(void *);
+    void (*freeData)(void *);
 } BST;
 
-BST* createBST(int (*cmp)(void*, void*), void (*print)(void*), void (*freeData)(void*));
-BSTNode* bstInsert(BSTNode* root, void* data, int (*cmp)(void*, void*));
-void* bstFind(BSTNode* root, void* data, int (*cmp)(void*, void*));
-void bstInorder(BSTNode* root, void (*print)(void*));
-void bstPreorder(BSTNode* root, void (*print)(void*));
-void bstPostorder(BSTNode* root, void (*print)(void*));
-void bstFree(BSTNode* root, void (*freeData)(void*));
+BST *createBST(int (*cmp)(void *, void *), void (*print)(void *), void (*freeData)(void *));
+BSTNode *bstInsert(BSTNode *root, void *data, int (*cmp)(void *, void *));
+void *bstFind(BSTNode *root, void *data, int (*cmp)(void *, void *));
+void bstInorder(BSTNode *root, void (*print)(void *));
+void bstPreorder(BSTNode *root, void (*print)(void *));
+void bstPostorder(BSTNode *root, void (*print)(void *));
+void bstFree(BSTNode *root, void (*freeData)(void *));
 
+// Custom
+void DeleteTree(BST *tree); // delete The tree and free the memory
 
 #endif
