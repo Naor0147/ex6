4f0d0afe24b40d25ae3b4d45f81b54ee90f4489d
Naor0147 <naoragam0147@gmail.com>
2026-01-09 17:29:08 +0000

    No changes detected

--- bst.c
+++ bst.c
@@ -1,9 +1,15 @@
 #include <stdlib.h>
 #include "bst.h"
 
+//Creates the Binary Tree , get compare ,print and free data Functions,set the root to null
+BST *createBST(int (*cmp)(void *, void *), void (*print)(void *), void (*freeData)(void *))
+{
+    BST *newTree = (BST *)malloc(sizeof(BST)); // allcoating meomry to the tree in the heap
 
-
-BST* createBST(int (*cmp)(void*, void*), void (*print)(void*), void (*freeData)(void*)){
-    BST * newTree=(BST * )malloc(sizeof(BST));//allcoating meomry to the tree in the heap 
-    ;
+    //Initialize the fields
+    newTree->compare = cmp;
+    newTree->print = print;
+    newTree->freeData = freeData;
+    newTree->root = NULL; //the root start as empty
+    return newTree;
 }
