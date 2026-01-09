2994fe23ef9cddbd87c0479d8a6c61dbbbd1e696
Naor0147 <naoragam0147@gmail.com>
2026-01-09 16:03:28 +0000

    Add implementation for binary search tree operations

--- bst.c
+++ bst.c
@@ -1,2 +1,8 @@
 #include <stdlib.h>
 #include "bst.h"
+
+
+
+BST* createBST(int (*cmp)(void*, void*), void (*print)(void*), void (*freeData)(void*)){
+    BST * newTree=(BST * )malloc(sizeof(BST));//creates the tree in the heap 
+}
